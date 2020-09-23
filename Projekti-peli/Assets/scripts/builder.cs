535b2f6fc58f5285bf622ee3e924a76039c81046
slimerplanet <eppuku@gmail.com>
2020-09-23 16:10:11 +0300

    added building

---
+++ Projekti-peli/Assets/scripts/builder.cs
@@ -0,0 +1,25 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class builder : MonoBehaviour
+{
+    [SerializeField] GameObject TowerPrefab;
+    [SerializeField] Camera cam;
+    [SerializeField] int BuildDistance;
+    [SerializeField] LayerMask PlacableArea;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            RaycastHit hit;
+            if(Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, BuildDistance, PlacableArea))
+            {
+                GameObject  obj = Instantiate(TowerPrefab, hit.point, Quaternion.identity);
+
+            }
+        }
+    }
+}
