add70ac12220d30aac235327c952f3e315690e66
slimerplanet <eppuku@gmail.com>
2020-10-08 17:11:02 +0300

    weapon switching

---
+++ Projekti-peli/Assets/scripts/weaponSwitching.cs
@@ -0,0 +1,67 @@
+﻿
+using UnityEngine;
+
+public class weaponSwitching : MonoBehaviour
+{
+
+    public int selectedWeapon = 0;
+
+    void Start()
+    {
+        selectWeapon();
+    }
+
+    void Update()
+    {
+        int previosSelectedWeapon = selectedWeapon;
+
+        if(Input.GetAxis("Mouse ScrollWheel") > 0f)
+        {
+            if (selectedWeapon >= transform.childCount - 1)
+                selectedWeapon = 0;
+            else                  
+                selectedWeapon++;
+        }
+
+        if (Input.GetAxis("Mouse ScrollWheel") < 0f)
+        {
+            if (selectedWeapon <= 0)
+                selectedWeapon = transform.childCount - 1;
+            else
+                selectedWeapon--;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            selectedWeapon = 0;
+        if (Input.GetKeyDown(KeyCode.Alpha2) && transform.childCount >= 2)
+            selectedWeapon = 1;
+        if (Input.GetKeyDown(KeyCode.Alpha3) && transform.childCount >= 3)
+            selectedWeapon = 2;
+        if (Input.GetKeyDown(KeyCode.Alpha4) && transform.childCount >= 4)
+            selectedWeapon = 3;
+        if (Input.GetKeyDown(KeyCode.Alpha5) && transform.childCount >= 5)
+            selectedWeapon = 4;
+
+
+
+
+        if (previosSelectedWeapon != selectedWeapon)
+            selectWeapon();
+
+    }
+
+
+    void selectWeapon()
+    {
+        int i = 0;
+        foreach(Transform weapon in transform)
+        {
+            if (i == selectedWeapon)
+                weapon.gameObject.SetActive(true);
+            else
+                weapon.gameObject.SetActive(false);
+
+            i++;
+        }
+    }
+}
