0be7541faf7e04255bbf819ffca4fde6eec7b078
slimerplanet <eppuku@gmail.com>
2020-10-08 17:34:51 +0300

    sword now work

---
+++ Projekti-peli/Assets/sword.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sword : MonoBehaviour
+{
+    public Animator animator;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse0)) {
+            animator.SetBool("attack", true);
+        }else {
+            animator.SetBool("attack", false);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+}
