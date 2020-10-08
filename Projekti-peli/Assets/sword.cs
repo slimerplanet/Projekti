3b042e8277a5cf2588cc8ae2d12da6dd22988a8f
slimerplanet <eppuku@gmail.com>
2020-10-08 18:13:32 +0300

    miekka

--- Projekti-peli/Assets/sword.cs
+++ Projekti-peli/Assets/sword.cs
@@ -5,18 +5,25 @@ using UnityEngine;
 public class sword : MonoBehaviour
 {
     public Animator animator;
-
+    public int damage;
+    [SerializeField] private bool attacking;
+    private void Start()
+    {
+        attacking = false;
+        animator.SetBool("attack", false);
+    }
     private void Update()
     {
         if(Input.GetKeyDown(KeyCode.Mouse0)) {
             animator.SetBool("attack", true);
-        }else {
+            attacking = true;
+        }else if(Input.GetKeyUp(KeyCode.Mouse0)){
             animator.SetBool("attack", false);
+            Invoke("setattackFalse", 0.25f);
         }
     }
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        
-    }
+    void setattackFalse() => attacking = false;
+
+
 }
