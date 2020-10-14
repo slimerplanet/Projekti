c05b2edb3e32def291a015f0c9a4d4082631cd8c
Swatpvp <67360231+Swatpvp@users.noreply.github.com>
2020-10-14 13:17:30 +0300

    Created Pistol

---
+++ Projekti-peli/Assets/scripts/Gun.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+   public int damage = 10;
+   public float range = 100f;
+
+   public Camera fpsCam;
+   public ParticleSystem muzzleFlash;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+
+    }
+
+    void Shoot ()
+    {
+        muzzleFlash.Play();
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+
+            enemy target = hit.transform.GetComponent<enemy>();
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+            }
+        }
+    }
+}
