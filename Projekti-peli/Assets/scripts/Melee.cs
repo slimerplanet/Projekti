a524a64254e771230b60771af008e4dabb8c61e1
slimerplanet <eppuku@gmail.com>
2020-10-19 16:36:46 +0300

    yes

--- Projekti-peli/Assets/scripts/Melee.cs
+++ Projekti-peli/Assets/scripts/Melee.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class Melee : MonoBehaviour
 {
     [SerializeField] Animator animator;
+    public LayerMask mask;
+    public Camera fpsCam;
+    public int damage = 25;
+    public int range = 5;
+
 
     private void Update()
     {
@@ -19,6 +24,15 @@ public class Melee : MonoBehaviour
     {
         animator.SetTrigger("attack");
 
-
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range, mask))
+        {
+            Debug.Log(hit.transform.name);
+            enemy enemy = hit.transform.GetComponentInParent<enemy>();
+            if (enemy != null)
+            {
+                enemy.TakeDamage(damage);
+            }
+        }
     }
 }
