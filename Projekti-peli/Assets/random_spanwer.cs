98153f4029ad4e77e05ced461116aea910e442d6
slimerplanet <eppuku@gmail.com>
2020-10-20 21:43:32 +0300

    added spawner

---
+++ Projekti-peli/Assets/random_spanwer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Animations.Rigging;
+using UnityEngine;
+
+public class random_spanwer : MonoBehaviour
+{
+    public GameObject prefab;
+
+    public float minTime;
+    public float maxtime;
+
+    float countdown;
+
+    void restartTimer()
+    {
+        countdown = Random.Range(minTime, maxtime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countdown -= Time.deltaTime;
+        if(countdown <= 0)
+        {
+            restartTimer();
+            Instantiate(prefab, transform.position, transform.rotation);
+        }
+    }
+}
