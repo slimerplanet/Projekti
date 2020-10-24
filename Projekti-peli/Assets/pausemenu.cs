81df75297f778657debffb64c1ab6e9fad16c27e
slimerplanet <eppuku@gmail.com>
2020-10-24 12:25:08 +0300

    things

---
+++ Projekti-peli/Assets/pausemenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pausemenu : MonoBehaviour
+{
+    public FirstPersonAIO controller;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(controller.controllerPauseState){
+            Time.timeScale = 0;
+        }
+        else {
+            Time.timeScale = 1;
+        }
+    }
+}
