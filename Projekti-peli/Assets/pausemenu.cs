c5849db4428f871a6a402f5bbc49b94c54649a2f
slimerplanet <eppuku@gmail.com>
2020-10-24 13:30:53 +0300

    player

--- Projekti-peli/Assets/pausemenu.cs
+++ Projekti-peli/Assets/pausemenu.cs
@@ -1,19 +1,51 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
 
 public class pausemenu : MonoBehaviour
 {
     public FirstPersonAIO controller;
+    public GameObject PauseMenuUI;
+
+    public bool paused;
 
     // Update is called once per frame
     void Update()
     {
-        if(controller.controllerPauseState){
-            Time.timeScale = 0;
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(paused)
+            {
+                Resume();
+            }else
+            {
+                Pause();
+            }
         }
-        else {
-            Time.timeScale = 1;
-        }
+
+
+    }
+
+    public void Pause()
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0;
+        paused = true;
+    }
+
+    public void Resume()
+    {
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1;
+        paused = false;
+    }
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("quit");
     }
 }
+
+
