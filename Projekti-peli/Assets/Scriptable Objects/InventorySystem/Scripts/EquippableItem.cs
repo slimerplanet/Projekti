bf2cd04aab5c044866e748225ba3e1f1dfd87caf
Swatpvp <67360231+Swatpvp@users.noreply.github.com>
2020-11-01 10:50:16 +0200

    Updates to inventory + cleanup

---
+++ Projekti-peli/Assets/Scriptable Objects/InventorySystem/Scripts/EquippableItem.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EquipmentType
+{
+    Helmet,
+    Chest,
+    Gloves,
+    Boots,
+    Weapon1,
+    Weapon2,
+}
+
+[CreateAssetMenu]
+public class EquippableItem : Item
+{
+    public int Damage;
+    public int DamageResistance;
+    public int Defence;
+    [Space]
+    public float DamagePrecentBonus;
+    public float DamageResistancePrecentBonus;
+    [Space]
+    public EquipmentType EquipmentType;
+}
