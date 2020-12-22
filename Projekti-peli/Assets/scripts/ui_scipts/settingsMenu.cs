e1ee6b925b74c15e49d8249b31657906162af702
Swatpvp <67360231+Swatpvp@users.noreply.github.com>
2020-12-22 12:11:37 +0200

    Quality / texture option phase 1

--- Projekti-peli/Assets/scripts/ui_scipts/settingsMenu.cs
+++ Projekti-peli/Assets/scripts/ui_scipts/settingsMenu.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class settingsMenu : MonoBehaviour
 {
 
     Resolution[] resolutions;
 
+    //public TMP_Dropdown resolutionDropdown;
+
+    public AudioMixer audioMixer;
     public TMP_Dropdown resolutionDropdown;
+    public TMP_Dropdown qualityDropdown;
+    public TMP_Dropdown textureDropdown;
+    public TMP_Dropdown aaDropdown;
+    public Slider volumeSlider;
+    float currentVolume;
 
     private void Start()
     {
@@ -20,7 +29,7 @@ public class settingsMenu : MonoBehaviour
         List<string> options = new List<string>();
 
 
-        int curResInd = 0;
+        int curResInd = 0; // current resolution index? (yeah, probably)
         for (int i = 0; i < resolutions.Length; i++)
         {
             string option = resolutions[i].width + " x " + resolutions[i].height;
@@ -37,14 +46,119 @@ public class settingsMenu : MonoBehaviour
         resolutionDropdown.RefreshShownValue();
     }
 
-    public void setfullScreen(bool isFullScreen)
+
+    // audio stuff
+    public void setVolume(float volume)
+    {
+        audioMixer.SetFloat("Volume", volume);
+        currentVolume = volume;
+    }
+
+    // graphics stuff
+
+    public void setfullScreen(bool isFullScreen) // is fullscreen?
     {
         Screen.fullScreen = isFullScreen;
     }
 
-    public void setResolution(int resolutionIndex)
+    public void setResolution(int resolutionIndex) // set resolution
     {
         Resolution res = resolutions[resolutionIndex];
         Screen.SetResolution(res.width, res.height, Screen.fullScreen);
     }
+
+    public void SetTextureQuality(int textureIndex) // set texture quality
+    {
+        QualitySettings.masterTextureLimit = textureIndex;
+        qualityDropdown.value = 5;
+    }
+
+    public void SetAntiAliasing(int aaIndex) // set anti aliasing
+    {
+        QualitySettings.antiAliasing = aaIndex;
+        qualityDropdown.value = 5;
+    }
+
+    public void SetQuality(int qualityIndex) // quality presets
+    {
+        if (qualityIndex != 6)
+            QualitySettings.SetQualityLevel(qualityIndex);
+        switch (qualityIndex)
+        {
+            case 0: // quality level - Ultra Performance
+                textureDropdown.value = 3;
+                aaDropdown.value = 0;
+                break;
+            case 1: // quality level - Performance
+                textureDropdown.value = 2;
+                aaDropdown.value = 0;
+                break;
+            case 2: // quality level - Balanced
+                textureDropdown.value = 1;
+                aaDropdown.value = 0;
+                break;
+            case 3: // quality level - Quality
+                textureDropdown.value = 0;
+                aaDropdown.value = 1;
+                break;
+            case 4: // quality level - Ultra Quality
+                textureDropdown.value = 0;
+                aaDropdown.value = 2;
+                break;
+        }
+    }
+
+    // setiing saving and loading stuff
+
+    public void SaveSettings() // saving
+    {
+        PlayerPrefs.SetInt("QualitySettingPreference",
+                   qualityDropdown.value);
+        PlayerPrefs.SetInt("ResolutionPreference",
+                   resolutionDropdown.value);
+        PlayerPrefs.SetInt("TextureQualityPreference",
+                   textureDropdown.value);
+        PlayerPrefs.SetInt("AntiAliasingPreference",
+                   aaDropdown.value);
+        PlayerPrefs.SetInt("FullscreenPreference",
+                   Convert.ToInt32(Screen.fullScreen));
+        PlayerPrefs.SetFloat("VolumePreference",
+                   currentVolume);
+    }
+
+    public void LoadSettings(int currentResolutionIndex) // loading
+    {
+        if (PlayerPrefs.HasKey("QualitySettingPreference"))
+            qualityDropdown.value =
+                         PlayerPrefs.GetInt("QualitySettingPreference");
+        else
+            qualityDropdown.value = 3;
+        if (PlayerPrefs.HasKey("ResolutionPreference"))
+            resolutionDropdown.value =
+                         PlayerPrefs.GetInt("ResolutionPreference");
+        else
+            resolutionDropdown.value = currentResolutionIndex;
+        if (PlayerPrefs.HasKey("TextureQualityPreference"))
+            textureDropdown.value =
+                         PlayerPrefs.GetInt("TextureQualityPreference");
+        else
+            textureDropdown.value = 0;
+        if (PlayerPrefs.HasKey("AntiAliasingPreference"))
+            aaDropdown.value =
+                         PlayerPrefs.GetInt("AntiAliasingPreference");
+        else
+            aaDropdown.value = 1;
+        if (PlayerPrefs.HasKey("FullscreenPreference"))
+            Screen.fullScreen =
+            Convert.ToBoolean(PlayerPrefs.GetInt("FullscreenPreference"));
+        else
+            Screen.fullScreen = true;
+        if (PlayerPrefs.HasKey("VolumePreference"))
+            volumeSlider.value =
+                        PlayerPrefs.GetFloat("VolumePreference");
+        else
+            volumeSlider.value =
+                        PlayerPrefs.GetFloat("VolumePreference");
+    }
 }
+
