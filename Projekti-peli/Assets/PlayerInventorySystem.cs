b511e40bc127f0e535066c9b34828219cea585e1
Swatpvp <67360231+Swatpvp@users.noreply.github.com>
2021-01-16 14:22:22 +0200

    Prep

--- Projekti-peli/Assets/PlayerInventorySystem.cs
+++ Projekti-peli/Assets/PlayerInventorySystem.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerInventory : MonoBehaviour
+public class PlayerInventorySystem : MonoBehaviour
 {
     [Header(" ")]
     public bool hasPistol;
